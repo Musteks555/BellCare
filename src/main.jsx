31b9c94a3f8b6d89df87fbf17e8d16b46fdad1b6
Musteks555 <artemmustek73@gmail.com>
2024-10-10 14:29:16 +0300

    add base structure, add reset styles

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
+
+// import store from "./redux/store.js";
+
+import App from "./components/App/App.jsx";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        {/* <HelmetProvider> */}
+        {/* <Provider store={store}> */}
+        {/* <BrowserRouter> */}
+        <App />
+        {/* </BrowserRouter> */}
+        {/* </Provider> */}
+        {/* </HelmetProvider> */}
+    </StrictMode>
+);
