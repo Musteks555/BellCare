159f15065544e66c26dc0d243ee15d5918ecb70c
Musteks555 <artemmustek73@gmail.com>
2024-10-17 20:12:03 +0300

    add loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import Loader from "../Loader/Loader";
 
 const Layout = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setLoading(true);
+
+        const timeout = setTimeout(() => {
+            setLoading(false);
+        }, 400);
+
+        return () => clearTimeout(timeout);
+    }, [location]);
+
     return (
         <>
             <Header />
 
-            <main>{children}</main>
+            <main>{loading ? <Loader /> : children}</main>
 
             <Footer />
         </>
