2dcafb825735c70fedc27d01f92525f8b4c54000
Musteks555 <artemmustek73@gmail.com>
2024-10-17 13:11:41 +0300

    add Delivery Information, fix WhoWeAre

--- src/pages/DeliveryInformation/DeliveryInformation.jsx
+++ src/pages/DeliveryInformation/DeliveryInformation.jsx
@@ -1,3 +1,4 @@
+import Container from "../../components/Container/Container";
 import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
 
 import css from "./DeliveryInformation.module.css";
@@ -5,9 +6,157 @@ import css from "./DeliveryInformation.module.css";
 const DeliveryInformation = () => {
     return (
         <>
-            <DocumentTitle>DeliveryInformation</DocumentTitle>
+            <DocumentTitle>Delivery Information</DocumentTitle>
 
-            <div>DeliveryInformation</div>
+            <section className={css.delivery}>
+                <Container>
+                    <h1 className={css.deliveryTitle}>DELIVERY INFORMATION</h1>
+
+                    <ul className={css.deliveryList}>
+                        <li className={css.deliveryItem}>
+                            <h3 className={css.deliveryItemTitle}>MAIN INFORMATION</h3>
+
+                            <div className={css.deliveryItemContainer}>
+                                <p className={css.deliveryItemText}>
+                                    At{" "}
+                                    <b className={css.deliveryNameColor}>
+                                        Bell<span>Care</span>
+                                    </b>
+                                    , our warehouse is stocked with a wide variety of popular medications, allowing us to process and
+                                    dispatch your order quickly.
+                                </p>
+
+                                <h4 className={css.deliveryItemSubTitle}>Our working hours are:</h4>
+
+                                <ul className={css.deliverySubList}>
+                                    <li className={css.deliverySubItem}>
+                                        <p className={css.deliveryItemText}>10AM - 6PM Monday - Thursday</p>
+                                    </li>
+
+                                    <li className={css.deliverySubItem}>
+                                        <p className={css.deliveryItemText}>10AM - 5PM Friday</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li className={css.deliveryItem}>
+                            <h3 className={css.deliveryItemTitle}>COURIER & DELIVERY TIME</h3>
+
+                            <div className={css.deliveryItemContainer}>
+                                <p className={css.deliveryItemText}>
+                                    We partner with internationally recognized courier companies to ensure secure and timely delivery. A
+                                    signature is required upon delivery for security.
+                                </p>
+
+                                <h4 className={css.deliveryItemSubTitle}>Delivery time frames are as follows:</h4>
+
+                                <ul className={css.deliverySubList}>
+                                    <li className={css.deliverySubItem}>
+                                        <p className={css.deliveryItemText}>
+                                            Orders placed between 9 AM to 5 PM EST will be shipped out the following business day.
+                                        </p>
+                                    </li>
+
+                                    <li className={css.deliverySubItem}>
+                                        <p className={css.deliveryItemText}>
+                                            Orders placed on Friday between 9 AM to 5 PM EST will be shipped out the first coming Monday.
+                                        </p>
+                                    </li>
+                                </ul>
+
+                                <p className={css.deliveryItemText}>
+                                    Shipping costs may vary or be waived depending on the order amount and product types.
+                                </p>
+
+                                <p className={css.deliveryItemText}>
+                                    <b>Please note</b>, shipping delays may occur due to unforeseen circumstances, including but not limited
+                                    to events such as war, strikes, riots, criminal acts, courier company issues, or weather conditions.{" "}
+                                </p>
+                            </div>
+                        </li>
+
+                        <li className={css.deliveryItem}>
+                            <h3 className={css.deliveryItemTitle}>LOST PARCELS & DELAYED SHIPMENTS</h3>
+
+                            <div className={css.deliveryItemContainer}>
+                                <p className={css.deliveryItemText}>
+                                    All orders are shipped via Express Mail based on the above delivery time frames.
+                                    <br />
+                                    <br />A package is considered delivered once we receive confirmation from the customer. In case of
+                                    delays, we will first attempt to locate the package and resolve the issue. If no resolution is possible,
+                                    we can send a replacement. However, the customer must agree to pay for both the replacement and the
+                                    delayed package, should the original arrive later.
+                                </p>
+                            </div>
+                        </li>
+
+                        <li className={css.deliveryItem}>
+                            <h3 className={css.deliveryItemTitle}>RETURN POLICY</h3>
+
+                            <div className={css.deliveryItemContainer}>
+                                <p className={css.deliveryItemText}>
+                                    <b className={css.deliveryNameColor}>
+                                        Bell<span>Care</span>
+                                    </b>{" "}
+                                    accepts returns within 8 days of product receipt, provided the return is due to one of the following
+                                    reasons:
+                                </p>
+
+                                <ul className={css.deliverySubList}>
+                                    <li className={css.deliverySubItem}>
+                                        <p className={css.deliveryItemText}>The product was incorrectly received.</p>
+                                    </li>
+
+                                    <li className={css.deliverySubItem}>
+                                        <p className={css.deliveryItemText}>he product arrived damaged.</p>
+                                    </li>
+                                </ul>
+
+                                <p className={css.deliveryItemText}>
+                                    If either of these conditions applies, please contact us by email or phone as listed on our website.
+                                    <br />
+                                    <br />
+                                    For valid returns, you will be offered either a replacement or a full refund for the returned products.
+                                    <br />
+                                    <b>No returns will be accepted after 8 days.</b>
+                                    <br />
+                                    <br />
+                                    Until further instructions are provided, please store the products according to the manufacturer&apos;s
+                                    recommended temperature and storage conditions.
+                                </p>
+                            </div>
+                        </li>
+
+                        <li className={css.deliveryItem}>
+                            <h3 className={css.deliveryItemTitle}>REFUND</h3>
+
+                            <div className={css.deliveryItemContainer}>
+                                <p className={css.deliveryItemText}>
+                                    Refunds will be processed to the original credit card used for payment. The timeframe for receiving the
+                                    refund depends on how quickly your bank processes it. The full amount of the order will be refunded.
+                                </p>
+                            </div>
+                        </li>
+
+                        <li className={css.deliveryItem}>
+                            <h3 className={css.deliveryItemTitle}>COMPLAINTS AND SUGGESTIONS</h3>
+
+                            <div className={css.deliveryItemContainer}>
+                                <p className={css.deliveryItemText}>
+                                    At{" "}
+                                    <b className={css.deliveryNameColor}>
+                                        Bell<span>Care</span>
+                                    </b>
+                                    , your satisfaction is our top priority! We welcome all feedback and suggestions. You can reach us via
+                                    email at <b>hello@bellcare-line.com</b> or contact our customer support team by phone. We&apos;re here
+                                    to assist and listen!
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </Container>
+            </section>
         </>
     );
 };
--- src/pages/WhoWeAre/WhoWeAre.jsx
+++ src/pages/WhoWeAre/WhoWeAre.jsx
@@ -51,72 +51,82 @@ const WhoWeAre = () => {
                         <li className={css.aboutItem}>
                             <h3 className={css.aboutItemTitle}>100% Authentic Products</h3>
 
-                            <p className={css.aboutItemText}>Access genuine medications sourced directly from leading manufacturers.</p>
+                            <div className={css.aboutItemContainer}>
+                                <p className={css.aboutItemText}>Access genuine medications sourced directly from leading manufacturers.</p>
+                            </div>
                         </li>
 
                         <li className={css.aboutItem}>
                             <h3 className={css.aboutItemTitle}>Delivery time frames are as follows:</h3>
 
-                            <ul className={css.aboutSubList}>
-                                <li className={css.aboutSubItem}>
-                                    <p className={css.aboutItemText}>
-                                        Orders placed between 9 AM to 5 PM EST will be shipped out the following business day.
-                                    </p>
-                                </li>
+                            <div className={css.aboutItemContainer}>
+                                <ul className={css.aboutSubList}>
+                                    <li className={css.aboutSubItem}>
+                                        <p className={css.aboutItemText}>
+                                            Orders placed between 9 AM to 5 PM EST will be shipped out the following business day.
+                                        </p>
+                                    </li>
 
-                                <li className={css.aboutSubItem}>
-                                    <p className={css.aboutItemText}>
-                                        Orders placed on Friday between 9 AM to 5 PM EST will be shipped out the first coming Monday.
-                                    </p>
-                                </li>
-                            </ul>
+                                    <li className={css.aboutSubItem}>
+                                        <p className={css.aboutItemText}>
+                                            Orders placed on Friday between 9 AM to 5 PM EST will be shipped out the first coming Monday.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
 
                         <li className={css.aboutItem}>
                             <h3 className={css.aboutItemTitle}>Only original products</h3>
 
-                            <p className={css.aboutItemText}>
-                                By purchasing wholesale medications from{" "}
-                                <b className={css.aboutNameColor}>
-                                    Bell<span>Care</span>
-                                </b>
-                                , your clinic can save thousands each month. We understand the importance of reliable medical supplies and
-                                offer a wide range of essential medications at highly competitive prices.
-                                <br />
-                                <br />
-                                Our inventory includes both well-known brand-name medications and generic alternatives, catering to various
-                                medical specialties. In the fast-paced wholesale market, we aim not only to compete but to lead in customer
-                                satisfaction, ensuring our success is measured by your satisfaction with our products.
-                                <br />
-                                <br />
-                                <b className={css.aboutNameColor}>
-                                    Bell<span>Care</span>
-                                </b>{" "}
-                                is committed to providing you with the most cost-effective pricing available.
-                            </p>
+                            <div className={css.aboutItemContainer}>
+                                <p className={css.aboutItemText}>
+                                    By purchasing wholesale medications from{" "}
+                                    <b className={css.aboutNameColor}>
+                                        Bell<span>Care</span>
+                                    </b>
+                                    , your clinic can save thousands each month. We understand the importance of reliable medical supplies
+                                    and offer a wide range of essential medications at highly competitive prices.
+                                    <br />
+                                    <br />
+                                    Our inventory includes both well-known brand-name medications and generic alternatives, catering to
+                                    various medical specialties. In the fast-paced wholesale market, we aim not only to compete but to lead
+                                    in customer satisfaction, ensuring our success is measured by your satisfaction with our products.
+                                    <br />
+                                    <br />
+                                    <b className={css.aboutNameColor}>
+                                        Bell<span>Care</span>
+                                    </b>{" "}
+                                    is committed to providing you with the most cost-effective pricing available.
+                                </p>
+                            </div>
                         </li>
 
                         <li className={css.aboutItem}>
                             <h3 className={css.aboutItemTitle}>Our Mission</h3>
 
-                            <p className={css.aboutItemText}>
-                                At{" "}
-                                <b className={css.aboutNameColor}>
-                                    Bell<span>Care</span>
-                                </b>
-                                , our mission is to be your go-to online destination for pharmaceutical needs. We are dedicated to providing
-                                tailored solutions for your business, ensuring that when it&apos;s time to restock, we&apos;re the first
-                                name that comes to mind.
-                            </p>
+                            <div className={css.aboutItemContainer}>
+                                <p className={css.aboutItemText}>
+                                    At{" "}
+                                    <b className={css.aboutNameColor}>
+                                        Bell<span>Care</span>
+                                    </b>
+                                    , our mission is to be your go-to online destination for pharmaceutical needs. We are dedicated to
+                                    providing tailored solutions for your business, ensuring that when it&apos;s time to restock, we&apos;re
+                                    the first name that comes to mind.
+                                </p>
+                            </div>
                         </li>
 
                         <li className={css.aboutItem}>
                             <h3 className={css.aboutItemTitle}>Our Vision</h3>
 
-                            <p className={css.aboutItemText}>
-                                Our vision is simple: to deliver the best for the best. We strive to support you in becoming a leading
-                                provider in your field by offering top-quality products and services.
-                            </p>
+                            <div className={css.aboutItemContainer}>
+                                <p className={css.aboutItemText}>
+                                    Our vision is simple: to deliver the best for the best. We strive to support you in becoming a leading
+                                    provider in your field by offering top-quality products and services.
+                                </p>
+                            </div>
                         </li>
                     </ul>
                 </Container>
