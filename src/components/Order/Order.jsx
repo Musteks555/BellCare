14a3c7aa0f8b098c711b1e1aa65466fd75559207
Musteks555 <artemmustek73@gmail.com>
2024-10-23 01:06:50 +0300

    add toast to order

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import toast, { Toaster } from "react-hot-toast";
 
 import { clearWishlist } from "../../redux/wishlist/slice";
 import { selectWishlist } from "../../redux/wishlist/selector";
@@ -42,6 +43,7 @@ const Order = ({ onClose, orderRef, toggleFavoritesModal }) => {
 
         if (!email && !phone) {
             setErrorMessage("Either email or phone must be provided.");
+            toast.error("Please provide either an email or a phone number.");
             return;
         }
 
@@ -57,59 +59,64 @@ const Order = ({ onClose, orderRef, toggleFavoritesModal }) => {
         dispatch(clearWishlist());
         toggleFavoritesModal();
         onClose();
+        toast.success("Order submitted successfully!");
     };
 
     return (
-        <div className={css.orderModal} onClick={handleBackdropClick} ref={orderRef}>
-            <div className={css.orderModalContent}>
-                <button className={css.orderModalCloseBtn} onClick={onClose}>
-                    <RxCross2 className={css.orderModalCloseBtnIcon} />
-                </button>
+        <>
+            <Toaster />
 
-                <h2 className={css.orderModalTitle}>Place an Order</h2>
-
-                <form className={css.orderModalForm} onSubmit={handleSubmit}>
-                    <label className={css.orderModalLabel}>
-                        Name:
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            className={css.orderModalInput}
-                            placeholder="Name"
-                        />
-                    </label>
-
-                    <label className={css.orderModalLabel}>
-                        E-mail:
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className={css.orderModalInput}
-                            placeholder="E-mail"
-                        />
-                    </label>
-
-                    <label className={css.orderModalLabel}>
-                        Phone:
-                        <input
-                            type="tel"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                            className={css.orderModalInput}
-                            placeholder="Phone"
-                        />
-                    </label>
-
-                    {errorMessage && <p className={css.errorMessage}>{errorMessage}</p>}
-
-                    <button type="submit" className={css.orderModalSubmitBtn}>
-                        SEND
+            <div className={css.orderModal} onClick={handleBackdropClick} ref={orderRef}>
+                <div className={css.orderModalContent}>
+                    <button className={css.orderModalCloseBtn} onClick={onClose}>
+                        <RxCross2 className={css.orderModalCloseBtnIcon} />
                     </button>
-                </form>
+
+                    <h2 className={css.orderModalTitle}>Place an Order</h2>
+
+                    <form className={css.orderModalForm} onSubmit={handleSubmit}>
+                        <label className={css.orderModalLabel}>
+                            Name:
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                className={css.orderModalInput}
+                                placeholder="Name"
+                            />
+                        </label>
+
+                        <label className={css.orderModalLabel}>
+                            E-mail:
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className={css.orderModalInput}
+                                placeholder="E-mail"
+                            />
+                        </label>
+
+                        <label className={css.orderModalLabel}>
+                            Phone:
+                            <input
+                                type="tel"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                className={css.orderModalInput}
+                                placeholder="Phone"
+                            />
+                        </label>
+
+                        {errorMessage && <p className={css.errorMessage}>{errorMessage}</p>}
+
+                        <button type="submit" className={css.orderModalSubmitBtn}>
+                            SEND
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
